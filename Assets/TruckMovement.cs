50ca828f8d9a85365127f609500c1f276d1ea3e2
asd <buronson@naver.com>
2018-12-08 06:47:09 +0900

    트럭 이동 구현

---
+++ Assets/TruckMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TruckMovement : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float moveSpeed = 40f;
+    private Vector3 dir;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        dir = new Vector3(0, 0, 1);
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        //transform.position = transform.forward * Time.deltaTime * moveSpeed;
+        transform.Translate(dir * Time.deltaTime * moveSpeed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        var rr = other.transform.GetChild(1);
+    }
+}
