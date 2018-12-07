2ffe68399f4c13240b805b667a77c9122a8cc410
찌찌뿌찌 <kjyong1983@gmail.com>
2018-12-08 05:08:47 +0900

    충돌해결중

--- Assets/PlayerInput.cs
+++ Assets/PlayerInput.cs
@@ -4,19 +4,21 @@ using UnityEngine;
 
 using HTC.UnityPlugin.Vive;
 
-public class VelocityTest : MonoBehaviour
+public class PlayerInput : MonoBehaviour
 {
     [SerializeField] private Transform player;
     [SerializeField] private Transform camera;
 
     [SerializeField] private HandRole leftHandRole;
     [SerializeField] private HandRole rightHandRole;
-    [SerializeField] private ControllerButton triggerButton = ControllerButton.Trigger;
+    private ControllerButton triggerButton = ControllerButton.Trigger;
 
     private Vector3 lastLeftPos;
     private Vector3 curLeftPos;
+
     private Vector3 lastRightPos;
     private Vector3 curRightPos;
+
     private Vector3 leftPunchDirection;
     private Vector3 rightPunchDirection;
 
@@ -26,22 +28,34 @@ public class VelocityTest : MonoBehaviour
     public Transform leftHand;
     public Transform rightHand;
 
+    [SerializeField] private Transform feetPos;
+
     [SerializeField] private float threshold = 2f;
     [SerializeField] private float leftMomentum;
     [SerializeField] private float rightMomentum;
-    [SerializeField] private bool runTrigger;
-    [SerializeField] private bool leftPunchTrigger;
-    [SerializeField] private bool rightPunchTrigger;
+
+    private bool runTrigger;
+    private bool leftPunchTrigger;
+    private bool rightPunchTrigger;
+
+    private bool leftJumpTrigger;
+    private bool rightJumpTrigger;
+
     [SerializeField] private float moveSpeed;
     [SerializeField] private float moveTime = 1.5f;
 
     private Collider punchCollider;
+
+    private Rigidbody rb;
+    [SerializeField] private float jumpPower;
+    [SerializeField] private bool isGrounded = true;
 
     // Use this for initialization
     private void Start()
     {
         lastLeftPos = VivePose.GetPose(leftHandRole).pos;
         lastRightPos = VivePose.GetPose(rightHandRole).pos;
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -56,54 +70,47 @@ public class VelocityTest : MonoBehaviour
         leftMomentum = leftPunchDirection.magnitude / Time.deltaTime;
         rightMomentum = rightPunchDirection.magnitude / Time.deltaTime;
 
-        //Debug.Log("velocity = " + momentum);
+        CheckPunch();
+        CheckJump();
+        CheckRun();
 
-        if (ViveInput.GetPress(leftHandRole, triggerButton) && leftMomentum > threshold)
-        {
-            leftPunchTrigger = true;
-        }
-        else
-        {
-            leftPunchTrigger = false;
-        }
+        lastLeftPos = curLeftPos;
+        lastRightPos = curRightPos;
+    }
 
-        if (ViveInput.GetPress(rightHandRole, triggerButton) && rightMomentum > threshold)
-        {
-            rightPunchTrigger = true;
-        }
-        else
-        {
-            rightPunchTrigger = false;
-        }
-
+    private void CheckRun()
+    {
         if ((!ViveInput.GetPress(leftHandRole, triggerButton) && leftMomentum > threshold) ||
             (!ViveInput.GetPress(rightHandRole, triggerButton) && rightMomentum > threshold))
         {
-            runTrigger = true;
-        }
-        else
-        {
-            runTrigger = false;
-        }
-
-        if (runTrigger)
-        {
             StartCoroutine("MoveForwardCoroutine");
-            runTrigger = false;
+        }
+    }
+
+    private void CheckJump()
+    {
+        if (ViveInput.GetPress(leftHandRole, triggerButton) && leftMomentum > threshold && Vector3.Angle(leftPunchDirection, Vector3.up) < 45)
+        {
+            rb.AddForce(Vector3.up * jumpPower);
         }
 
-        if (leftPunchTrigger)
+        if (ViveInput.GetPress(rightHandRole, triggerButton) && rightMomentum > threshold && Vector3.Angle(rightPunchDirection, Vector3.up) < 45)
+        {
+            rb.AddForce(Vector3.up * jumpPower);
+        }
+    }
+
+    private void CheckPunch()
+    {
+        if (ViveInput.GetPress(leftHandRole, triggerButton) && leftMomentum > threshold)
         {
             StartCoroutine("LeftPunchCoroutine");
         }
 
-        if (rightPunchTrigger)
+        if (ViveInput.GetPress(rightHandRole, triggerButton) && rightMomentum > threshold)
         {
             StartCoroutine("RightPunchCoroutine");
         }
-
-        lastLeftPos = curLeftPos;
-        lastRightPos = curRightPos;
     }
 
     private IEnumerator MoveForwardCoroutine()
@@ -132,5 +139,13 @@ public class VelocityTest : MonoBehaviour
         punch.isTrigger = false;
         yield return new WaitForSeconds(0.5f);
         punch.isTrigger = true;
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.layer == 1 << LayerMask.NameToLayer("Ground"))
+        {
+            isGrounded = true;
+        }
     }
 }
